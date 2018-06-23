694c7d57a154062b0d987c30654a0f05ffbe06b2
Kennan Smith <kennan.smith1@gmail.com>
2018-06-23 16:11:26 -0600

    have a working, styled list and a selected hero info section once a hero is clicked

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HEROES } from '../mock-heroes';
+
 // @ means it's a "decorator"
 @Component({
   selector: 'app-heroes',
@@ -8,13 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  // the caps lock HEROES is just from the tutorial - it doesn't have to be all caps
+  // tutorial instructions - "add a heroes property to the class that exposes these heroes for binding"
+  heroes = HEROES
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
-  };
-
+  }
 
   constructor() { }
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   ngOnInit() {
   }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+// here we are importing the hero class and then exporting 
+// an array that follows that convention - it's a failsafe - 
+// -bad inputs for data will not go through
+
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
